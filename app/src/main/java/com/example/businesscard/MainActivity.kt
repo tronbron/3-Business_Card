986fb470c1156629104cf7017a0e26f271a627c4
James <jamespbenedict96@gmail.com>
2023-12-21 18:18:49 +0800

    Achieved minimum requirement.

--- app/src/main/java/com/example/businesscard/MainActivity.kt
+++ app/src/main/java/com/example/businesscard/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.businesscard
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.shrinkHorizontally
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
@@ -10,6 +11,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -17,21 +19,32 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Email
+import androidx.compose.material.icons.rounded.Phone
+import androidx.compose.material.icons.rounded.Share
+import androidx.compose.material3.Divider
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.blur
 import androidx.compose.ui.draw.paint
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.BitmapPainter
 import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.layout.HorizontalAlignmentLine
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.businesscard.ui.theme.BusinessCardTheme
 
 class MainActivity : ComponentActivity() {
@@ -52,88 +65,77 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun BusinessCard() {
-    Column (modifier = Modifier
-        .fillMaxSize()
-        .background(Color(0xff009d94)),
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color(0xff009d94)),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
-    ){
+    ) {
+        Spacer(modifier = Modifier.padding(bottom = 100.dp))
         Image(
             painter = painterResource(R.drawable.android_logo),
             contentDescription = "null",
             contentScale = ContentScale.Fit,
             modifier = Modifier
-                .width(150.dp)
-                .height(150.dp)
+                .width(120.dp)
+                .height(120.dp)
         )
         Text(
             text = "James PB",
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Bold,
             modifier = Modifier
         )
         Text(
             text = "MacGyver",
+            fontSize = 16.sp,
             modifier = Modifier
         )
-        }
-    Column (modifier = Modifier
-        .padding(0.dp, 0.dp, 0.dp, 50.dp)
-        .fillMaxSize(),
-        verticalArrangement = Arrangement.Bottom,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ){
-        Text(
+        Spacer(modifier = Modifier.padding(bottom = 300.dp))
+        Contact(
             text = "+00 (00) 000 000",
-            modifier = Modifier
-                .padding(8.dp)
-                .paint(painterResource(R.drawable.baseline_call_black_24dp))
-                .weight(1f, fill = false)
-            )
-            Text(
+            icon = Icons.Rounded.Phone
+        )
+        Contact(
             text = "@socialmediahandle",
-            modifier = Modifier
-                .padding(8.dp)
-                .paint(painterResource(R.drawable.baseline_email_black_24dp))
-            )
-            Text(
+            icon = Icons.Rounded.Share
+        )
+        Contact(
             text = "email@domain.com",
-            modifier = Modifier
-                .padding(8.dp)
-                .paint(painterResource(R.drawable.baseline_share_black_24dp))
-            )
-        }
+            icon = Icons.Rounded.Email
+        )
     }
+}
+
+@Composable
+fun Contact(
+    text: String, icon: ImageVector
+){
+    Row(
+        modifier = Modifier
+            .padding(10.dp)
+            .fillMaxWidth(0.6f)
+    ) {
+        Icon(
+            imageVector = icon,
+            contentDescription = null,
+            modifier = Modifier.weight(1f)
+        )
+        Text(
+            text = text,
+            modifier = Modifier
+                .align(Alignment.CenterVertically)
+                .weight(3f)
+
+        )
+    }
+}
 
 @Preview(showBackground = false)
 @Composable
-fun GreetingPreview() {
+fun GreetingCard() {
     BusinessCardTheme {
         BusinessCard()
     }
 }
-
-
-/**@Composable
-fun BusinessCard2(
-modifier: Modifier = Modifier
-){
-Column (
-modifier = Modifier
-.fillMaxSize()
-.background(Color(0xff3bc639)),
-verticalArrangement = Arrangement.Bottom,
-horizontalAlignment = Alignment.CenterHorizontally
-) {
-Text(
-text = "number",
-modifier = modifier
-)
-Text(
-text = "@handle",
-modifier = modifier
-)
-Text(
-text = "email",
-modifier = modifier
-)
-}
-}**/
